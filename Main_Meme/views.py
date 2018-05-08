6e3dab8cea7a9103d4246240f453edcc35b7dce4
Ferran Martinez <fmp4@alumnes.udl.cat>
2018-05-08 10:33:56 +0200

    Home page shows memes

--- Main_Meme/views.py
+++ Main_Meme/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from Main_Meme.models import *
 
 # Create your views here.
+
+def home(request):
+
+    memes = Meme.objects.all()
+    context = {}
+    context['memes'] = memes
+    template = 'home.html'
+    return render(request, template, context)
--- MemeProject/urls.py
+++ MemeProject/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.views.generic import TemplateView
+from Main_Meme.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls'), name="login"),
-    path('', TemplateView.as_view(template_name="home.html"), name="home")
-]
+    path('', home, name="home")
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
